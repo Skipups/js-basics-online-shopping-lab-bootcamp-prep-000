a652ee947517747576e29b4e965ea3c5d521103d
Svetlana R <svetlana.rovinsky@gmail.com>
2019-05-04 19:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
-
+    for (let i=0; i<cart.length; i++){
+      cart.splice(i);
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
